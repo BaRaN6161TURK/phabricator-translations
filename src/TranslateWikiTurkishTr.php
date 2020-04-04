e556e111daabc62ecda5bc423623cb1709520ad7
Baran <53825631+BaRaN6161TURK@users.noreply.github.com>
2020-04-04 10:21:56 +0300

    New language
    This language is fully translated.

---
+++ src/TranslateWikiTurkishTr.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * TranslateWiki Locale for Turkish localization in Phabricator
+ */
+final class TranslateWikiDanishDa extends PhutilLocale {
+    public function getLocaleCode() {
+        return 'tr';
+    }
+
+    public function getLocaleName() {
+        return pht('Turkish');
+    }
+}
